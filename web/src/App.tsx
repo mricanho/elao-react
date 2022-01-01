e1ddf7e2260d4e22655f407ac84c4d8428ac8fb4
mricanho <mricanho@gmail.com>
2022-01-01 10:34:47 -0600

    Fix index.css error in app.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,7 +4,7 @@ import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
-import './index.tsx'
+import './index.css'
 
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
